aedb83cea1e4dfbaee1264f87c7a3bd75867ac00
JanekRoos <janek.roos@yandex.ru>
2023-10-05 08:43:29 +0300

    foundry

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
